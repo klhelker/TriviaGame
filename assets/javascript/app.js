94060e5a65913fe91b84fe3a054ccd59b5a6dd38
Kristin Helker <klhelker@gmail.com>
2020-01-11 15:39:52 -0600

    coded in js and jquery to start running game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,50 @@
+var count = 0;
+var questions=[{
 
-$('.mybtn').on('click', function(event) {
-    event.preventDefault(); 
-    var url = $(this).data('target');
-    location.replace(url);
-});
+question: "what is color of sky",
+answer: "blue",
+answers:  ["blue", "yellow", "green", "red"]
+}]
+// console.log("script");
+// this onclick is based on the ID for Start button//
+//.on = listener
+$("#start").on("click",(event)=>{
+//  prevent default//
+   event.preventDefault()
+   console.log("click working");
+   printQuestions()
 
+})
 
-.
-$("button").click(addTask);
-
-// Add a listener to the document.
-// It should keep an ear out for a click on elements with an id of "delete".
-// We can't use .click, because the element is dynamically created.
-$(document).on("click", "#delete", removeTask);
-
-// When a user presses any key on their keyboard,
-$("input").keypress(function(event) {
-
-  // listen to see that key was "enter."
-  if (event.which === 13) {
-
-    // If so, run addTask.
-    addTask();
+function printQuestions(){
+  $("#game-container").empty();
+  var gameContainer= $("#game-container")
+  var question = $("<h3>").text(questions[count].question)
+  //appending div to container
+  gameContainer.append(question);
+  //Looping through object at answers key at integer count
+  //Go to whatever integer index is in questions
+  //find answers key
+  //lopp through to print answers
+  var answerList=$("<ul>")
+  //this loop prints each button to a list so that we can add it underneath our answer
+  
+  for(i=0;i<questions[count].answers.length;i++){
+    //ListItem holds each item that is a radio button
+    var listItem = $("<li>").append("<button>").attr("type", "radio").attr("name", "answer").addClass("answer-button").text(questions[count].answers[i]);
+    //append each list item with a button to the answerList
+    answerList.append(listItem)
   }
-});
+  //append finalized list to game container
+  gameContainer.append(answerList);
+
+  //create submit button
+  var submitButton = $("<button>").attr("id", "submit");
+  //append button to gameContainer
+  gameContainer.append(submitButton);
+
+
+count += 1;
+console.log("count",count);
+
+}
